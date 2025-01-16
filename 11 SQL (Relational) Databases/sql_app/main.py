f47685e939bfaaac4271437aa8b3128d02666ab3
Nguyen Tien Trong <trongbg2692004@gmail.com>
2025-01-16 23:51:47 +0700

    update

--- 11 SQL (Relational) Databases/sql_app/main.py
+++ 11 SQL (Relational) Databases/sql_app/main.py
@@ -14,7 +14,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/users/", response_model=schemas.User)
+@app.post("/users/", response_model=schemas.User, tags= ['User'])
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, user_email=user.email)
     if db_user:
@@ -22,13 +22,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users/", response_model=list[schemas.User])
+@app.get("/users/", response_model=list[schemas.User], tags= ['User'])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
+@app.get("/users/{user_id}", response_model=schemas.User, tags= ['User'])
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if not db_user:
@@ -36,7 +36,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+@app.post("/users/{user_id}/items/", response_model=schemas.Item, tags=['Item'])
 def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if not db_user:
@@ -44,33 +44,33 @@ def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = D
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@app.get("/items/", response_model=list[schemas.Item])
+@app.get("/items/", response_model=list[schemas.Item], tags=['Item'])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
 
-@app.put('/users/{user_id}', response_model=schemas.User)
+@app.put('/users/{user_id}', response_model=schemas.User, tags= ['User'])
 async def update_user_email(user_id : int , user_update: schemas.UserCreate, db: Session= Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if not db_user:
         raise HTTPException(status_code=404, detail='User does not exist')
     return crud.update_user_email(db=db, user_id=user_id, user_update=user_update )
 
-@app.put('/items/{item_id}', response_model=schemas.Item)
+@app.put('/items/{item_id}', response_model=schemas.Item, tags=['Item'])
 async def update_item( item_id : int , item_update: schemas.ItemBase, db:Session = Depends(get_db)):
     db_item = crud.get_item(db=db, item_id=item_id)
     if not db_item:
         raise HTTPException(status_code=404, detail='Item does not exist')
     return crud.update_item(db=db, item_id=item_id, item_update=item_update)
 
-@app.delete('/users/{user_id}', response_model=schemas.User)
+@app.delete('/users/{user_id}', response_model=schemas.User, tags= ['User'])
 async def delete_user(user_id : int , db:Session = Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if not db_user:
         raise HTTPException(status_code=404, detail='User does not exist')
     return crud.delete_user(user_id=user_id, db=db)
 
-@app.delete('/items/{item_id}', response_model=schemas.Item)
+@app.delete('/items/{item_id}', response_model=schemas.Item, tags=['Item'])
 async def delete_item(item_id : int , db:Session= Depends(get_db)):
     db_item = crud.get_item(db=db, item_id=item_id)
     if not db_item:
--- 2 path operation/main.py
+++ 2 path operation/main.py
@@ -1,12 +1,65 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Optional
 '''
-get : retrieve 
-post : create 
-put : update
-delete : delete
+GET : retrieve 
+POST : create 
+PUT : update
+DELETE : delete
 '''
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return 'Hello World'
+
+students = {
+    1: {
+        "name": "Nguyen Tien Trong",
+        "age": 21,
+        "sex": "male"
+    },
+    2: {
+        "name": "Nguyen Tien Long",
+        "age": 21,
+        "sex": "Female"
+    }
+}
+
+# Path parameter
+@app.get('/get_student/{student_id}')
+async def get_student(student_id: int = Path(..., description='The ID of the student you want to view', gt=0 , lt=3)):
+    return students[student_id]
+
+# Query parameter
+@app.get('/get_student_by_name/')
+async def get_student( name: Optional[str] = None):
+    for student_id in students:
+        if students[student_id]['name'] == name:
+            return students[student_id]
+    return {'Data': 'Not Found'}
+
+# combine path and query parameter
+@app.get('/get_student/{student_id}/')
+async def get_student(student_id: int = Path(..., description='The ID of the student you want to view', gt=0 , lt=3), name: Optional[str] = None):
+    if name:
+        for student_id in students:
+            if students[student_id]['name'] == name:
+                return students[student_id]
+        return {'Data': 'Not Found'}
+    return students[student_id]
+
+# POST method
+
+from pydantic import BaseModel
+
+class Student(BaseModel):
+    name: str
+    age: int
+    sex : str
+
+@app.post('/create_student/{student_id}')
+async def create_student( student_id: int, student: Student ):
+    if student_id in students:
+        return {'Error': 'Student already exists'}
+    students[student_id] = student
+    return students[student_id]
